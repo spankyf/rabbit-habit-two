1bebc4e1df4bec1df105f441b332c6997b314646
spankyf <deanflanagan@gmail.com>
2020-07-28 13:41:51 -0400

    Auto seed and beginning overview pug

--- models/sleep.js
+++ models/sleep.js
@@ -1,23 +1,84 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Sleep extends Model {
     /**
      * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
+     * This method is not a part of DataTypes lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
       // define association here
     }
-  };
-  Sleep.init({
-    date: DataTypes.DATEONLY
-  }, {
-    sequelize,
-    modelName: 'Sleep',
-  });
+  }
+  Sleep.init(
+    {
+      date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+      bedtime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      sleeptime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      waketime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      dreams_from_three: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 0,
+          max: 3,
+        },
+      },
+      interruptions: {
+        type: DataTypes.INTEGER,
+      },
+      pee: {
+        type: DataTypes.INTEGER,
+      },
+      rating_from_five: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+          max: 5,
+        },
+      },
+      duration: {
+        type: DataTypes.VIRTUAL,
+        get() {
+          const lostTime = this.pee * 10 + this.interruptions * 20;
+          const duration = this.waketime - this.sleeptime;
+          return (duration - lostTime) / 60000;
+        },
+      },
+
+      interruptions: {
+        type: DataTypes.INTEGER,
+      },
+      pee: {
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Sleep",
+    }
+  );
   return Sleep;
 };
--- server.js
+++ server.js
@@ -3,17 +3,31 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
 const morgan = require("morgan");
+const db = require("./models");
+const fs = require("fs");
 const sleepRouter = require("./routes/sleepRoutes");
+const catchAsync = require("./utils/catchAsync");
 
-app.use(morgan("dev"));
+const seedData = catchAsync(async (req, res) => {
+  const seed = await db.Sleep.bulkCreate(
+    JSON.parse(
+      fs.readFileSync(path.join(__dirname, "utils", "data", "sleep.json"))
+    )
+  );
+  console.log("Sleep data seeded");
+});
+
+db.sequelize.sync({ force: true }).then(() => {
+  seedData();
+  console.log("Drop and re-sync db.");
+});
 
 app
   .use(express.static(path.join(__dirname, "public")))
+  .use(morgan("dev"))
+  .use("/sleep", sleepRouter)
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "pug");
-
-// app.get("/", (req, res) => res.send("Hello World!"));
-app.use("/sleep", sleepRouter);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
