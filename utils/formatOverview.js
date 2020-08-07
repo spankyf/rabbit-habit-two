6be55caba2eec3d2972c1b1cb10f9fbb79c73dad
spankyf <deanflanagan@gmail.com>
2020-08-07 16:38:10 -0400

    Get logic for passing working

--- utils/formatOverview.js
+++ utils/formatOverview.js
@@ -1,9 +1,22 @@
 module.exports = (arr) => {
   arr.forEach((el) => {
+    if (el.format == "hours" || el.format == "minutes") {
+      console.log(typeof el.dbQuery.goalResult);
+      console.log(typeof el.number);
+      console.log(el.number, el.dbQuery.goalResult);
+    }
+
+    if (el.operator == ">=") {
+      el.passing = el.number >= el.dbQuery.goalResult;
+    } else if (el.operator == "<=") {
+      el.passing = el.number <= el.dbQuery.goalResult;
+    }
+
     if (el.format == "percentage") {
-      el.dbQuery.goalResult = parseFloat(el.dbQuery.goalResult * 100).toFixed(
-        2
-      );
+      el.dbQuery.goalResult =
+        parseFloat(el.dbQuery.goalResult * 100)
+          .toFixed(2)
+          .toString() + "%";
     } else if (el.format == "hours" || el.format == "minutes") {
       var n = new Date(0, 0);
 
