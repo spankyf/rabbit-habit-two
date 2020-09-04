6b4d07e8856023a9463a382b2cfd3c17c6089913
spankyf <deanflanagan@gmail.com>
2020-09-04 11:15:12 -0400

    Frontend of overview upated

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const viewsController = require("../controllers/viewsController");
 const router = express.Router();
 
-router.get("/", viewsController.addSandwich, viewsController.getOverview);
+router.get("/", viewsController.getOverview);
 
 module.exports = router;
--- utils/formatOverview.js
+++ utils/formatOverview.js
@@ -1,31 +1,49 @@
-const compareGoals = require("./compareGoals");
-
 module.exports = (arr) => {
   // console.log(arr);
-  // console.log("                                ****");
-  arr.forEach((el) => {
-    // console.log(el);
-    if (el.operator == ">=") {
-      el.passing = el.number >= el.dbQuery.goalResult;
-    } else if (el.operator == "<=") {
-      el.passing = el.number <= el.dbQuery.goalResult;
+  var operators = {
+    "<=": function (a, b) {
+      return a <= b;
+    },
+    ">=": function (a, b) {
+      return a >= b;
+    },
+  };
+
+  function convertNumToTime(number) {
+    // Separate the int from the decimal part
+    var hour = Math.floor(number);
+    var decpart = number - hour;
+
+    var min = 1 / 60;
+    // Round to nearest minute
+    decpart = min * Math.round(decpart / min);
+
+    var minute = Math.floor(decpart * 60) + "";
+
+    // Add padding if need
+    if (minute.length < 2) {
+      minute = "0" + minute;
     }
 
-    if (el.format == "percentage") {
-      el.dbQuery.goalResult =
-        parseFloat(el.dbQuery.goalResult * 100)
-          .toFixed(2)
-          .toString() + "%";
-    } else if (el.format == "hours" || el.format == "minutes") {
-      var n = new Date(0, 0);
+    // Concate hours and minutes
+    time = hour + ":" + minute;
 
-      n.setSeconds(+el.dbQuery.goalResult.toString() * 60 * 60);
-      el.dbQuery.goalResult = n.toTimeString().slice(0, 5);
+    return time;
+  }
+  arr.forEach((el) => {
+    el.passing = operators[el.operator](el.dbQuery.goalResult, el.number);
+
+    if (el.format == "time") {
+      el.number = convertNumToTime(el.number);
+      el.current = convertNumToTime(el.dbQuery.goalResult);
+    } else if (el.format == "percentage") {
+      el.number = (el.number * 100).toFixed(2) + "%";
+      el.current = (el.dbQuery.goalResult * 100).toFixed(2) + "%";
     } else {
-      el.dbQuery.goalResult = parseFloat(el.dbQuery.goalResult).toFixed(2);
+      el.current = el.dbQuery.goalResult.toFixed(2);
     }
   });
-  //return arr;
-  const res = compareGoals(arr);
-  return res;
+  return arr;
+  // const res = compareGoals(arr);
+  // return res;
 };
